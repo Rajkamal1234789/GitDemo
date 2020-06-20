ebd71f1d3535b731bd32418895993996027a8813
rajkamal <prakarmkl9849@gmail.com>
2020-06-20 20:54:23 +0530

    second commit from american guy

--- src/test/java/rajkamal/Raaj.java
+++ src/test/java/rajkamal/Raaj.java
@@ -3,7 +3,7 @@ package rajkamal;
 public class Raaj {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("mamidala");
 
 	}
 
--- src/test/java/rajkamal/sample.java
+++ src/test/java/rajkamal/sample.java
@@ -5,6 +5,9 @@ public class sample {
 	public static void main(String[] args) {
 		
 		System.out.println("rajkmal");
+		System.out.println("rajkmal1");
+		System.out.println("rajkmal2");
+		System.out.println("rajkmal3");
 
 	}
 
