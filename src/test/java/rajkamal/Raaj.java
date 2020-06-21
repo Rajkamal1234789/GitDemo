ac4bbad0783c2d2cb024639d5ccfba7044380d9b
rajkamal <prakarmkl9849@gmail.com>
2020-06-21 12:51:16 +0530

    asian guy architect

--- src/test/java/rajkamal/Raaj.java
+++ src/test/java/rajkamal/Raaj.java
@@ -4,7 +4,10 @@ public class Raaj {
 
 	public static void main(String[] args) {
 		System.out.println("mamidala");
-
+		System.out.println("mamidala1");
+		System.out.println("mamidal2");
+		System.out.println("mamidala3");
+		System.out.println("mamidala4");
 	}
 
 }
